0a64d0203fbb6013e9a726f0d6a4289e72165b87
AJ Bahnken <aj.bahnken@procore.com>
2017-05-15 17:33:34 -0700

    Initial commit

---
+++ rip.c
@@ -0,0 +1,106 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// TODO
+// 2. Options:
+//      * -f, --fields is a comma seperated list of fields to print
+
+void usage() {
+  //TODO
+  printf("rip - like cut, but more like what I actually want cut to do\n");
+  printf("------------------------------------------------------------\n");
+  printf("USAGE: rip [OPTIONS] [FILE]\n");
+  printf("Options:");
+  printf("  -f - Field you ");
+}
+
+void process_line(char *instr, char *delim, int field) {
+  char *val;
+  int i = 0;
+
+  // TODO: Strip out newline from instr if it exists.
+
+  while ((val = strsep(&instr, delim)) != NULL) {
+    if (!strcmp(val, "")) {
+      continue;
+    }
+    if (field >= 0) {
+      if (i == field) {
+        printf("%s\n", val);
+      }
+    } else {
+      printf("[%d] %s\n", i, val);
+    }
+
+    i++;
+  }
+}
+
+void process_file(char *filename, char *delim, int field) {
+  FILE *fp;
+  char *line = NULL;
+  size_t size;
+
+  if(!strcmp(filename, "-")) {
+    fp = stdin;
+  } else {
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+      printf("Failed to open file: %s\n", filename);
+      abort();
+    }
+  }
+
+  while (getline(&line, &size, fp) != -1) {
+    process_line(line, delim, field);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char *delim = "|";
+  int field = -1;
+  int c;
+  char *end;
+  int index;
+
+  while ((c = getopt(argc, argv, "hd:f:")) != -1) {
+    switch(c) {
+      case 'd':
+        delim = optarg;
+        break;
+      case 'f':
+        field = strtol(optarg, &end, 10);
+        break;
+      case 'h':
+        usage();
+        break;
+      case '?': // TODO: Does this work as expected?
+        if (optopt == 'd' || optopt == 'f') {
+          fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+        } else if (isprint(optopt)) {
+          fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+        } else {
+          fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+        }
+        usage();
+        return 1;
+      default:
+        abort();
+    }
+  }
+
+  // TODO: Support more than one field
+  if (optind < argc) {
+    for (index = optind; index < argc; index++) {
+      process_file(argv[index], delim, field);
+    }
+  } else {
+    char *f = "-";
+    process_file(f, delim, field);
+  }
+
+  return 0;
+}
